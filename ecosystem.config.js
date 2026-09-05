c9bb0ef3ab35f91dca114ad213e0c441c6379e55
gaurav012jais <gjaiswalk057@gmail.com>
2026-09-05 13:26:31 +0530

    chore(pm2): dynamically load PORT from process.env and dotenv in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   apps: [
     {
@@ -10,11 +12,11 @@ module.exports = {
       max_memory_restart: '500M',
       env: {
         NODE_ENV: 'development',
-        PORT: 5000,
+        PORT: process.env.PORT,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 5000,
+        PORT: process.env.PORT,
       },
       error_file: './logs/pm2-err.log',
       out_file: './logs/pm2-out.log',
